56855ad29afbf9be52fc191bf20f1a8f6382e324
Devin <40843522+devthedevel@users.noreply.github.com>
2022-11-29 18:09:32 -0330

    Added error handling

--- 2022/1/index.ts
+++ 2022/1/index.ts
@@ -4,10 +4,12 @@ export async function input(lines: string[]): Promise<any> {
 
 export async function one(input: string[]) {
     console.log(`input for part 1: ${input}`)
-    return 'test'
+    throw new Error('failed')
+
+
 }
 
 export async function two(input: string[]) {
     console.log(`input for part 2: ${input}`)
-    return 10
+    return 'test'
 }
--- 2022/index.ts
+++ 2022/index.ts
@@ -9,9 +9,11 @@ interface Options {
 
 interface Module {
     input: (lines: string[]) => Promise<[any, any]>;
-    one: (input: any) => Promise<any>;
-    two: (input: any) => Promise<any>;
+    one: AocFunction;
+    two: AocFunction;
 }
+
+type AocFunction = (input: any) => Promise<any>;
 
 const header = colors.bold.white;
 const solution = colors.green;
@@ -33,13 +35,16 @@ async function main(options: Options, day: number) {
     const parts = ['Part One', 'Part Two'].filter((_, i) => i + 1 !== options.exclude);
     const inputFile = options.sample ? 'sample.txt' : 'input.txt';
 
+    const modulePath = `./${day}/index.ts`;
+    const inputPath = `./${day}/${inputFile}`;
+
     console.log(`${header('Running Day:')} ${day} (${parts.join(' & ')})`);
     console.log(`${header('Input File:')} ${inputFile}`)
 
     try {
-        const module: Module = await import(`./${day}/index.ts`);
+        const module: Module = await import(modulePath);
 
-        const inputLines = await readInputFile(`./${day}/${inputFile}`);
+        const inputLines = await readInputFile(inputPath);
         const parsedInput = await module.input(inputLines);
 
         if (options.exclude !== 1) {
@@ -50,26 +55,42 @@ async function main(options: Options, day: number) {
             await runPart(parts[1], module.two, parsedInput[1], options.bench)
         }
     } catch (e) {
-        console.error(e)
+        if (e.code === 'ERR_MODULE_NOT_FOUND') {
+            console.error(`Unable to open module ${modulePath}`)
+        } else if (e instanceof Deno.errors.NotFound) {
+            console.error(`Unable to open input file ${inputFile}`)
+        }
+        Deno.exit(1);
     }
+
+    Deno.exit();
 }
 
-async function runPart(title: string, fn: any, input: any, shouldBench?: boolean): Promise<void> {
+async function runPart(title: string, fn: AocFunction, input: any, shouldBench?: boolean): Promise<void> {
     console.log(header(`\n -= ${title} =-`))
 
-    if (shouldBench) {
-        performance.mark('start');
-    }
+    try {
+        if (!fn) {
+            console.error('Cannot find function');
+            return
+        }
+    
+        if (shouldBench) {
+            performance.mark('start');
+        }
+        const answer = await fn(input);
 
-    const answer = await fn(input);
-    console.log(`${header('Solution:')} ${solution(String(answer))}`)
+        console.log(`${header('Solution:')} ${solution(String(answer))}`)
 
-    if (shouldBench) {
-        performance.mark('end');
-        const measure = performance.measure('bench', 'start', 'end');
-        console.log(`${header('Bench:')} ${bench(String(measure.duration) + ' ms')}`);
-        performance.clearMarks('start');
-        performance.clearMarks('end');
+        if (shouldBench) {
+            performance.mark('end');
+            const measure = performance.measure('bench', 'start', 'end');
+            console.log(`${header('Bench:')} ${bench(String(measure.duration) + ' ms')}`);
+            performance.clearMarks('start');
+            performance.clearMarks('end');
+        }
+    } catch (error) {
+        console.error(error)
     }
 }
 
