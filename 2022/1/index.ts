73be2925b2f319dc3704ce05acee594b4df54162
Devin <40843522+devthedevel@users.noreply.github.com>
2022-12-01 08:37:57 -0330

    2022 Day 1

--- 2022/1/index.ts
+++ 2022/1/index.ts
@@ -1,15 +1,40 @@
-export async function input(lines: string[]): Promise<any> {
-    return [lines, lines]
+type Input = number[][];
+
+export async function input(lines: string[]): Promise<[Input, any]> {
+    const input: Input = [];
+
+    let temp: number[] = [];
+    lines.forEach((line, i) => {
+        if (line === '' || i === lines.length) {
+            input.push(temp);
+            temp = [];
+        } else if (i === lines.length - 1) {
+            input.push([Number(line)])
+        } else {
+            temp.push(Number(line));
+        }
+    })
+
+    return [input, input]
 }
 
-export async function one(input: string[]) {
-    console.log(`input for part 1: ${input}`)
-    throw new Error('failed')
+export async function one(input: Input) {
+    let max = 0;
 
+    input.forEach(elf => {
+        const sum = elf.reduce((acc, curr) => acc + curr, 0);
+        if (sum > max) {
+            max = sum;
+        }
+    })
 
+    return max
 }
 
-export async function two(input: string[]) {
-    console.log(`input for part 2: ${input}`)
-    return 'test'
+export async function two(input: Input) {
+    const summedInput = input.map(elf => elf.reduce((acc, curr) => acc + curr, 0));
+
+    summedInput.sort((a, b) => a - b);
+
+    return summedInput.slice(-3).reduce((acc, curr) => acc + curr, 0)
 }
