79f77aaac667f905174b9a7592af26da1bfd2c01
Devin <40843522+devthedevel@users.noreply.github.com>
2022-12-03 17:45:47 -0330

    Day 3

---
+++ 2022/3/index.ts
@@ -0,0 +1,71 @@
+type ItemList = string[];
+
+const LOWER_OFFSET = 96;        // UTF-16 code for 'a' starts at 96
+const UPPER_OFFSET = 65 - 27;   // UTF-16 code for 'A' starts at 65, priority offset by 27
+
+function isUppercase(char: string): boolean {
+    const _char = char;
+
+    return char === _char.toUpperCase();
+}
+
+function getPriority(char: string): number {
+    return char.charCodeAt(0) - (isUppercase(char) ? UPPER_OFFSET : LOWER_OFFSET);
+}
+
+export async function input(lines: string[]): Promise<[ItemList[], ItemList[]]> {
+    const items = lines.map(line => line.split(''));
+
+    return [items, items];
+}
+
+export async function one(input: ItemList[]): Promise<number> {
+    return input.reduce((acc, curr) => {
+        const firstCompartment = new Set([...curr.slice(0, curr.length / 2)]);
+        const secondCompartment = new Set([...curr.slice(curr.length / 2)]);
+
+        const common: string[] = [];
+
+        firstCompartment.forEach(item => {
+            if (secondCompartment.has(item)) {
+                common.push(item);
+            }
+        })
+
+        return acc + common.reduce((comAcc, comCurr) => {
+            return comAcc + getPriority(comCurr);
+        }, 0);
+    }, 0);
+}
+
+export async function two(input: ItemList[]): Promise<number> {
+    let sum = 0;
+    let numIterations = input.length / 3;
+
+    for (let i = 0; i < numIterations; i++) {
+        const idx = i * 3;
+        
+        const group1 = new Set([...input[idx]]);
+        const group2 = new Set([...input[idx + 1]]);
+        const group3 = new Set([...input[idx + 2]]);
+
+        let badge = '';
+        const common: string[] = [];
+
+        group1.forEach(item => {
+            if (group2.has(item)) {
+                common.push(item);
+            }
+        });
+
+        common.forEach(item => {
+            if (group3.has(item)) {
+                badge = item;
+            }
+        });
+
+        sum += getPriority(badge);
+    }
+
+    return sum;
+}
